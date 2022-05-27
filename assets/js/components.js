1034f7efa789ef242dda178142f099fde5a993b6
Shuya Setsuna <88424960+shuyshuys@users.noreply.github.com>
2022-05-27 01:23:24 +0000

    update

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,3 +1,5 @@
+// alert('test')
+
 class Header extends HTMLElement {
     connectedCallback(){
         this.innerHTML = `
@@ -5,10 +7,9 @@ class Header extends HTMLElement {
             <div class="header-top">
                 <div class="container">
                 <div class="row">
-                    <div class="col-lg-3 col-md-12 left-item">
+                    <div class="col-lg-7 col-md-12 left-item">
                     <ul>
-                        <li>
-                        <i class="fas fa-envelope-square"></i> sinematoofficial@gmail.com</li>
+                        <li><i class="fas fa-envelope-square"></i>sinematoofficial@gmail.com</li>
                         <li><i class="fas fa-phone-square"></i> 085816081685</li>
                     </ul>
                     </div>
@@ -42,19 +43,32 @@ class Header extends HTMLElement {
                     <ol class="sub-menu">
                         <li class="menu-item"><a href="sistem-integrasi.html">Sistem Integrasi</a></li>
                         <li class="menu-item"><a href="data-center.html">Data Center</a></li>
+                        <li class="menu-item"><a href="portofolio-proyek-sistem-integtasi.html">Portofolio Proyek Sistem Integrasi</a></li>
                     </ol>
                     </li>
                     <li class="menu-item">
                     <a href="konsultasi.html">Konsultasi & Audit</a>
                     <ol class="sub-menu">
                         <li class="menu-item"><a href="desain-infrastruktur-ti.html">Desain Infrastruktur Teknologi Informasi</a></li>
-                        <li class="menu-item"><a href="#0">Keamanan Informasi</a></li>
-                        <li class="menu-item"><a href="#0">Tata Kelola Teknologi Informasi</a></li>
-                        <li class="menu-item"><a href="#0">Master Plan</a></li>
-                        <li class="menu-item"><a href="#0">Pelatihan IT</a></li>
+                        <li class="menu-item"><a href="keamanan-informasi.html">Keamanan Informasi</a></li>
+                        <li class="menu-item"><a href="tata-kelola-ti.html">Tata Kelola Teknologi Informasi</a></li>
+                        <li class="menu-item"><a href="master-plan.html">Master Plan</a></li>
+                        <li class="menu-item"><a href="pelatihan-ti.html">Pelatihan IT</a></li>
+                        <li class="menu-item"><a href="portofolio.html">Portofolio Konsultasi dan Audit</a></li>
+                    </ol>
+                    </li>
+                    <li class="menu-item">
+                    <a href="artikel.html">Artikel</a>
+                    <ol class="sub-menu">
+                        <li class="menu-item"><a href="tata-kelola-ti.html">Tata Kelola TI</a></li>
+                        <li class="menu-item"><a href="keamanan-informasi.html">Keamanan Informasi</a></li>
+                        <li class="menu-item"><a href="it-master-plan.html">IT Master Plan</a></li>
+                        <li class="menu-item"><a href="sistem-ntegrasi.html">Sistem Integrasi</a></li>
+                        <li class="menu-item"><a href="portofolio-proyek.html">Portofolio Proyek</a></li>
                     </ol>
                     </li>
                     <li class="menu-item"><a href="kontak.html">Kontak</a></li>
+                        <li class="menu-item"><a href="https://backend-it-consultant.shuyshuys.repl.co">Admin</a></li>
                 </ol>
             </nav>
         </header>
@@ -63,3 +77,5 @@ class Header extends HTMLElement {
 }
 
 customElements.define('header-component', Header);
+
+// alert("test")
